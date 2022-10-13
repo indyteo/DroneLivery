b3f18a4788cfa850d780bb4ec380809cd49117c8
julienleows <julienleo.weisse@gmail.com>
2022-10-13 14:59:22 +0200

    add: delivery script

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,70 +1,13 @@
-﻿using Events;
+﻿using System;
+using Common;
+using Events;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
-	[SerializeField] private Transform droneContainer;
-
-	private int _meters;
-	private int _delivered;
-	private bool _delivering;
-
-	private int meters {
-		get => this._meters;
-		set {
-			this._meters = value;
-			EventManager.Instance.Raise(new MetersUpdatedEvent(value));
-		}
+public class Delivery : MonoBehaviour {
+	private void OnCollisionEnter(Collision collision) {
+		EventManager.Instance.Raise(new DeliverStartEvent());
+		SfxManager.Instance.PlaySfx2D("DeliverStart");
+		Destroy(this.gameObject);
 	}
-
-	private int delivered {
-		get => this._delivered;
-		set {
-			this._delivered = value;
-			EventManager.Instance.Raise(new DeliveredUpdatedEvent(value));
-		}
-	}
-
-	private bool delivering {
-		get => this._delivering;
-		set {
-			this._delivering = value;
-			EventManager.Instance.Raise(new DeliveringUpdatedEvent(value));
-		}
-	}
-
-	protected void Awake() {
-		EventManager.Instance.AddListener<GamePlayEvent>(this.OnGamePlay);
-		EventManager.Instance.AddListener<DeliverStartEvent>(this.OnDeliverStart);
-		EventManager.Instance.AddListener<DeliverEndEvent>(this.OnDeliverEnd);
-		EventManager.Instance.AddListener<DroneCrashedEvent>(this.OnDroneCrashEvent);
-	}
-
-	private void OnDestroy() {
-		EventManager.Instance.RemoveListener<GamePlayEvent>(this.OnGamePlay);
-		EventManager.Instance.RemoveListener<DeliverStartEvent>(this.OnDeliverStart);
-		EventManager.Instance.RemoveListener<DeliverEndEvent>(this.OnDeliverEnd);
-		EventManager.Instance.RemoveListener<DroneCrashedEvent>(this.OnDroneCrashEvent);
-	}
-
-	private void OnGamePlay(GamePlayEvent e) {
-		this.meters = 0;
-		this.delivered = 0;
-		Instantiate(Resources.Load<GameObject>("Drone"), this.droneContainer);
-		EventManager.Instance.Raise(new DroneSpawnedEvent());
-	}
-
-	private void OnDeliverStart(DeliverStartEvent e) {
-		this.delivering = true;
-	}
-
-	private void OnDeliverEnd(DeliverEndEvent e) {
-		this.delivering = false;
-		if (e.Success)
-			this.delivered++;
-	}
-
-	private void OnDroneCrashEvent(DroneCrashedEvent e) {	
-		EventManager.Instance.Raise(new GameOverEvent(GameManager.ComputeScore(this.delivered, this.meters)));
-	}
-	
 }
